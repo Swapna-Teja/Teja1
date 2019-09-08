7cf0b8a0950b6f5010e7ac57cda78c384b175006
user <user@192.168.0.25>
2019-09-08 14:22:05 +0530

    commit

---
+++ Test1/src/NewTest/Git2.java
@@ -0,0 +1,29 @@
+package NewTest;
+
+
+
+import org.testng.annotations.Test;
+
+public class Git2 {
+	@Test
+	public void testA()
+	{
+		System.out.println("First Testcase");
+	}
+	@Test(dependsOnMethods= {"login"})
+	public void testB()
+	{
+		System.out.println("Second Testcase");
+	}
+	@Test
+	public void login() {
+		System.out.println("this login functionality");
+	}
+	@Test
+	public void logoff() {
+		System.out.println("logout functionality");
+	}
+	
+
+
+}
